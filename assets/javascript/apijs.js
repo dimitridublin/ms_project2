9463fa4e20f6fea0c2afedf22be9b211efe87590
dimitridublin <dimitris@doctor.com>
2021-05-11 13:45:01 +0000

    added maps API and co-ordinates of interest

--- assets/javascript/apijs.js
+++ assets/javascript/apijs.js
@@ -1,35 +1,14 @@
-/* var xhr = new XMLHttpRequest();
-var data;
-
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
-
-function setData(jsonData) {
-    data = jsonData;
-}
-
-xhr.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        setData(JSON.parse(this.responseText));
-    };
-}
-
-console.log(data);
-
-*/
-// creating the map
+// creating the google map, used "How to Google Maps" by Eamonn Smyth for tips and advice on how to formulate code below
 const properties = {
-    "lat": 12,
-    "lng": 100
+    "lat": 38.7229839697771,
+    "lng": -9.139263109412974
 }
+initMap();
 
-function myMap() {
+function initMap() {
     const mapParameters = {
         center: properties,
-        zoom: 15,
+        zoom: 13,
     };
     const map = new google.maps.Map(document.getElementById("map"), mapParameters);
-
 }
-
-myMap()
