3b846f7bb9a4a28941305e41f115d4636f652315
dimitridublin <dimitris@doctor.com>
2021-05-15 19:13:44 +0000

    added function to confirm form submission on about page

--- assets/javascript/aboutForm.js
+++ assets/javascript/aboutForm.js
@@ -1,3 +1,10 @@
+// about page enquiry form button change to confirm form submission
+function formSubmitted() {
+    buttonSubmitted = document.getElementById("submit");
+    buttonSubmitted.style.backgroundColor = "red";
+    buttonSubmitted.textContent = "Enquiry Form Submitted";
+};
+
 // about page enquiry form submission script, code adapted from example provided in CI "Sending Emails Using EmailJS" tutorial
 function respondMail(aboutForm) {
     emailjs.send("service_schssol", "template_hjzgsre", {
@@ -16,5 +23,6 @@ function respondMail(aboutForm) {
                 console.log("FAILED", error);
             }
         );
+    formSubmitted();
     return false; // Block browser from reloading page on form submission
 }
