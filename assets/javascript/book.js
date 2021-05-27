56673f12f1d4f45d7c3b7154dcee12a5fb6cb065
dimitridublin <dimitris@doctor.com>
2021-05-27 19:20:26 +0000

    added function when Book form is submitted

--- assets/javascript/book.js
+++ assets/javascript/book.js
@@ -5,5 +5,16 @@ function bookFunction() {
         return;
     }
 }
-
 window.onload = bookFunction();
+
+// Function to handle form submit in absence of functionality to provide user with available packages (feature to be implemented)
+function formSubmit(event) {
+    event.preventDefault();
+    let htmlBook = `<div class="col-12 d-block alert alert-warning mt-5">
+    <h1 class="text-center">Online Booking System currently Under Construction</h1>
+    <p class="text-center">Please contact our office by telephone: 01-6070808 or email: hello@booterstowntravel.ie</p></div>`
+    $("section").append(htmlBook);
+};
+
+let form = document.getElementById("book-form");
+form.addEventListener("submit", formSubmit);
