73932a37ff096f23e1be99f99039f73cb76269a1
dimitridublin <dimitris@doctor.com>
2021-05-30 15:55:34 +0000

    book form hides on submission

--- assets/javascript/book.js
+++ assets/javascript/book.js
@@ -9,10 +9,12 @@ window.onload = bookFunction();
 
 // Function to handle form submit in absence of functionality to provide user with available packages (feature to be implemented)
 function formSubmit(event) {
+    //prevent default action on form submission
     event.preventDefault();
     let htmlBook = `<div class="col-12 d-block alert alert-warning mt-5">
     <h1 class="text-uppercase text-center">Online Booking System currently Under Construction</h1>
     <p class="text-center">Please contact our office by telephone: 01-6070808 or email: hello@booterstowntravel.ie</p></div>`;
+    $("#book-form").hide("fast");
     $("section").append(htmlBook);
 }
 
